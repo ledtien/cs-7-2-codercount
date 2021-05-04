8cde10fb79b69ceeac276195d117a30ba5f4c3ab
Tien <ldtien.1993@gmail.com>
2021-05-04 16:24:27 +0700

    codercount

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { Nav, Navbar, Form, FormControl, Button } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const state = useSelector((state) => state);
+  const { count, boxColors } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const currentUser = useSelector((state) => state.currentUser);
+
+  const onSignIn = (e) => {
+    e.preventDefault();
+    dispatch({ type: "SIGN_IN", payload: email });
+  };
+
+  function onChangeBoxColor(e, idx) {
+    if (!idx) {
+      dispatch({ type: "CHANGE_COLOR", payload: e.target.value });
+    } else {
+      dispatch({ type: "CHANGE_COLOR_BOX", idx, payload: e.target.value });
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">CoderCount</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          {currentUser ? (
+            <Button
+              type="submit"
+              variant="outline-danger"
+              onClick={() => dispatch({ type: "SIGN_OUT" })}
+            >
+              Sign-out {currentUser}?
+            </Button>
+          ) : (
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Email"
+                className="mr-sm-2"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Button
+                type="submit"
+                variant="outline-success"
+                onClick={onSignIn}
+              >
+                Sign In
+              </Button>
+            </Form>
+          )}
+        </Navbar.Collapse>
+      </Navbar>
+
+      <div className="App">
+        <div className="bg-secondary">
+          <h1>Total input: {count}</h1>
+          <input
+            onChange={(e) =>
+              dispatch({ type: "CHANGE_COLOR", payload: e.target.value })
+            }
+          ></input>
+          <button
+            className="m-2"
+            onClick={() => dispatch({ type: "INCREMENT" })}
+          >
+            Increment
+          </button>
+          <button onClick={() => dispatch({ type: "DECREMENT" })}>
+            Decrement
+          </button>
+        </div>
+        {Array.from(Array(state.count)).map((c, idx) => {
+          const boxColor = boxColors[idx] || state.defaultColor;
+          return (
+            <div
+              className="box border p-4 m-1 d-flex justify-content-between"
+              style={{ backgroundColor: boxColor, fontSize: 20 }}
+              key={idx + 1}
+            >
+              Box {idx + 1}
+              <input onChange={(e) => onChangeBoxColor(e, idx)}></input>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/redux/store.js
@@ -0,0 +1,51 @@
+import { createStore } from "redux";
+
+const initialState = {
+  count: 1,
+  defaultColor: "red",
+  boxColors: [],
+  currentUser: "",
+};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      if (state.count === 1) return state;
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case "CHANGE_COLOR":
+      return {
+        ...state,
+        defaultColor: action.payload,
+      };
+
+    case "CHANGE_COLOR_BOX":
+      const boxColors = state.boxColors;
+      boxColors[action.idx] = action.payload;
+      return {
+        ...state,
+        boxColors,
+      };
+
+    case "SIGN_IN":
+      return { ...state, currentUser: action.payload };
+    case "SIGN_OUT":
+      return { ...state, currentUser: "" };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
